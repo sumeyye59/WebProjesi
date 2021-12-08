08c23ba29a71582ca2b17d40fe2caf91f2771eea
sumeyye59 <63192409+sumeyye59@users.noreply.github.com>
2021-12-08 14:33:39 +0300

    .

---
+++ KitapKesifleri/Models/Translator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitapKesifleri.Models
+{
+    public class Translator
+    {
+        public int Id { get; set; }
+    }
+}
